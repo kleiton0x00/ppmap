fdecf69f33f7b8334f710b961255b0dfbee6e112
kleiton0x00 <37262788+kleiton0x00@users.noreply.github.com>
2022-04-07 21:55:31 +0200

    Update setup.sh
    Install Chromium automatically during the setup process

--- setup.sh
+++ setup.sh
@@ -1,13 +1,21 @@
-echo [+] Creating a Go module
+if (cat /etc/os-release | grep Kali) ; then
+    echo "[+] Installing Chromium in Kali"
+    sudo apt-get install -y chromium
+else
+    echo "[+] Installing Chromium in Debian-based linux"
+    sudo apt-get install -y chromium-browser
+fi
+
+echo "[+] Creating a Go module"
 go mod init ppmap
 
-echo [+] Installing the required library
+echo "[+] Installing the required library"
 go get github.com/chromedp/chromedp
 
-echo [+] Building the binary
+echo "[+] Building the binary"
 go build
 
-echo [+] Moving the compiled binary to /usr/bin
+echo "[+] Moving the compiled binary to /usr/bin"
 sudo mv ppmap /usr/bin
 
-echo [+] Setup finished!
+echo "[+] Setup finished!"
